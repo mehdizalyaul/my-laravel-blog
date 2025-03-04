919e7546a4955dce5cd993d049923e729b5a7ba5
Mehdi ZALYAUL <mehdi.zalyaul@gmail.com>
2025-03-04 23:44:36 +0000

    refactoring seeder and factory for category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,16 +1,21 @@
 <?php
 
-
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Database\Seeders\PostSeeder;
+use Database\Seeders\CommentSeeder;
+use Database\Seeders\CategorySeeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        // Call the seeders
         $this->call(UserSeeder::class);
+        $this->call(PostSeeder::class);
         $this->call(CommentSeeder::class);
+        $this->call(CategorySeeder::class);
     }
 }
