10b9532166bb09d5d433dacecad67a856205fdbe
Mehdi ZALYAUL <mehdi.zalyaul@gmail.com>
2025-03-22 22:49:33 +0000

    Integrating slug using laravel-sluggable package

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,12 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Post extends Model
 {
     use HasFactory;
+    use HasSlug;
 
-    protected $fillable = ['title', 'content', 'user_id','category_id','image'];
+    protected $fillable = ['title', 'content', 'user_id','category_id','image','slug'];
+
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')  // Generate slug from title
+            ->saveSlugsTo('slug');  // Save slug in 'slug' column
+    }
+
 
     public function user()
     {
